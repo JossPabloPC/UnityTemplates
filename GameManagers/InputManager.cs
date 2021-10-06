99049d57695f8a0923b22b4625f0e3c202113241
JossPabloPC <51423507+JossPabloPC@users.noreply.github.com>
2021-10-06 17:40:45 +0200

    Managers addes
    Added an:
    -Game manager
    -Input manager
    -Singleton class

---
+++ GameManagers/InputManager.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InputManager : MonoBehaviour
+{
+    public delegate void OnStart();
+    public event OnStart OnPlayerUp;
+    public event OnStart OnPlayerDown;
+
+    public static InputManager Instance
+    {
+        get;
+        private set;
+    }
+
+    public void Awake()
+    {
+        Instance = this;
+    }
+
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.Q))
+            OnPlayerUp();
+        else if (Input.GetKey(KeyCode.A))
+            OnPlayerDown();
+    }
+}
