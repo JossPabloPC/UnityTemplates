57b1a5869207d733f6b4877d157dfcdbcb4e7a7b
JossPabloPC <51423507+JossPabloPC@users.noreply.github.com>
2021-11-23 12:28:45 +0100

    New singleton
    Persisten singleton for multiple herency

--- GameManagers/InputManager.cs
+++ GameManagers/InputManager.cs
@@ -4,6 +4,9 @@ using UnityEngine;
 
 public class InputManager : MonoBehaviour
 {
+    public delegate void MoveEvent(Vector2 value);
+    public event MoveEvent CameraRotate;
+
     public delegate void OnStart();
     public event OnStart OnPlayerUp;
     public event OnStart OnPlayerDown;
@@ -21,6 +24,10 @@ public class InputManager : MonoBehaviour
 
     void Update()
     {
+        Vector2 camera = new Vector2(Input.GetAxis("Mouse X"), Input.GetAxis("Mouse Y"));
+        CameraRotate(camera);
+
+
         if (Input.GetKey(KeyCode.Q))
             OnPlayerUp();
         else if (Input.GetKey(KeyCode.A))
