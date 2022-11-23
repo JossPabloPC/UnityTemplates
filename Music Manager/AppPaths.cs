590706fada733e5396b972b8bc437837af994427
JossPabloPC <51423507+JossPabloPC@users.noreply.github.com>
2022-11-22 20:48:42 -0600

    Update AppPaths.cs

--- Music Manager/AppPaths.cs
+++ Music Manager/AppPaths.cs
@@ -8,3 +8,9 @@ public class AppPaths
     public static readonly string PATH_RESOURCE_SFX     = "Audio/SFX/";
     public static readonly string PATH_RESOURCE_MUSIC   = "Audio/Music/";
 }
+
+public class AppPlayerPrefKeys
+{
+    public static readonly string MUSIC_VOLUME = "MUSIC_VOLUME";
+    public static readonly string VFX_VOLUME = "VFX _VOLUME ";
+}
